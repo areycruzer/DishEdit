1db295659f0c843fe6d6448a7eb2de3a86211d71
swyam sharma <swyam7@gmail.com>
2026-07-18 19:06:18 +0530

    Initial Commit

---
+++ DishEdit/DishEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishEditApp.swift
+//  DishEdit
+//
+//  Created by Swyam Sharma on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct DishEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DishEditTests/DishEditTests.swift
@@ -0,0 +1,19 @@
+//
+//  DishEditTests.swift
+//  DishEditTests
+//
+//  Created by Swyam Sharma on 18/07/26.
+//
+
+import Testing
+@testable import DishEdit
+
+struct DishEditTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
